144e5f1f8906620500c0ad684ad80be9f07bc7e9
Saumitro Dasgupta <ethereon@xinverse.com>
2015-11-27 18:53:15 -0800

    Implemented NodeDispatch + LayerAdapter + KernelParameters

--- kaffe/layers.py
+++ kaffe/layers.py
@@ -72,4 +72,65 @@ class NodeKind(LayerType):
         except NotImplementedError:
             raise KaffeError('Output shape computation not implemented for type: %s'%node.kind)
 
+class NodeDispatchError(KaffeError): pass
 
+class NodeDispatch(object):
+    def get_handler_name(self, node_kind):
+        if len(node_kind)<=4:
+            # A catch-all for things like ReLU and tanh
+            return node_kind.lower()
+        # Convert from CamelCase to under_scored
+        name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', node_kind)
+        return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
+
+    def get_handler(self, node_kind, prefix):
+        name = self.get_handler_name(node_kind)
+        name = '_'.join((prefix, name))
+        if hasattr(self, name):
+            return getattr(self, name)
+        raise NodeDispatchError('No handler found for node kind: %s (expected: %s)'%(node_kind, name))
+
+class LayerAdapter(NodeDispatch):
+    def __init__(self, layer, kind):
+        self.layer = layer
+        self.kind = kind
+
+    def parameters_convolution(self):
+        return self.layer.convolution_param
+
+    def parameters_pooling(self):
+        return self.layer.pooling_param
+
+    def parameters_inner_product(self):
+        return self.layer.inner_product_param
+
+    def parameters_concat(self):
+        return self.layer.concat_param
+
+    def parameters_lrn(self):
+        return self.layer.lrn_param
+
+    @property
+    def parameters(self):
+        handler = self.get_handler(self.kind, 'parameters')
+        return handler()
+
+    @property
+    def kernel_parameters(self):
+        assert self.kind in (NodeKind.Convolution, NodeKind.Pooling)
+        params = self.parameters
+        k_h = params.kernel_h or params.kernel_size
+        k_w = params.kernel_w or params.kernel_size
+        s_h = params.stride_h or params.stride
+        s_w = params.stride_w or params.stride
+        p_h = params.pad_h or params.pad
+        p_w = params.pad_h or params.pad
+        return KernelParameters(k_h, k_w, s_h, s_w, p_h, p_w)
+
+KernelParameters = namedtuple('KernelParameters',
+                              ['kernel_h',
+                              'kernel_w',
+                              'stride_h',
+                              'stride_w',
+                              'pad_h',
+                              'pad_w'])
