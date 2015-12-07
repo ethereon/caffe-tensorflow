364ae5e74c6224225ca449ca7073497f94bc4934
Saumitro Dasgupta <ethereon@xinverse.com>
2015-12-06 22:04:43 -0800

    Updated test script to use model specific parameters

--- test.py
+++ test.py
@@ -7,36 +7,35 @@ import numpy as np
 import tensorflow as tf
 import examples
 
-BATCH_SIZE      = 25
-PRE_CROP_SIZE   = 256
-IMAGE_SIZE      = 224
-NUM_CHANNELS    = 3
-NUM_LABELS      = 1000
-
 class ImageNet(object):
-    def __init__(self, val_path, data_path, mean=None):
+    def __init__(self, val_path, data_path, model):
         gt_lines = open(val_path).readlines()
         gt_pairs = [line.split() for line in gt_lines]
         self.image_paths = [os.path.join(data_path, p[0]) for p in gt_pairs]
         self.labels = np.array([int(p[1]) for p in gt_pairs])
-        if mean is None:
-            self.mean = np.array([103.939, 116.779, 123.68])
+        self.model = model    
+        self.mean = np.array([104., 117., 124.])
 
     def read_image(self, path):
         img = cv2.imread(path)
         h, w, c = np.shape(img)
+        scale_size = self.model.scale_size
+        crop_size = self.model.crop_size
         assert c==3
-        aspect = float(w)/h
-        if w<h:
-            resize_to = (PRE_CROP_SIZE, int((1.0/aspect)*PRE_CROP_SIZE))
+        if self.model.isotropic:
+            aspect = float(w)/h
+            if w<h:
+                resize_to = (scale_size, int((1.0/aspect)*scale_size))
+            else:
+                resize_to = (int(aspect*scale_size), scale_size)
         else:
-            resize_to = (int(aspect*PRE_CROP_SIZE), PRE_CROP_SIZE)
+            resize_to = (scale_size, scale_size)        
         img = cv2.resize(img, resize_to)
-        h, w, c = img.shape
-        delta = IMAGE_SIZE/2
-        img = img[(h/2)-delta:(h/2)+delta, (w/2)-delta:(w/2)+delta, :]
         img = img.astype(np.float32)
         img -= self.mean
+        h, w, c = img.shape
+        ho, wo = ((h-crop_size)/2, (w-crop_size)/2)
+        img = img[ho:ho+crop_size, wo:wo+crop_size, :]
         img = img[None, ...]
         return img
 
@@ -49,19 +48,19 @@ class ImageNet(object):
     def __len__(self):
         return len(self.labels)
 
-def test_imagenet(Net, data_path, val_path, images_path, top_k=5):
-    test_data   = tf.placeholder(tf.float32, shape=(BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS))
-    test_labels = tf.placeholder(tf.int32, shape=(BATCH_SIZE,))
-    net         = Net({'data':test_data})
+def test_imagenet(model, data_path, val_path, images_path, top_k=5):
+    test_data   = tf.placeholder(tf.float32, shape=(model.batch_size, model.crop_size, model.crop_size, model.channels))
+    test_labels = tf.placeholder(tf.int32, shape=(model.batch_size,))
+    net         = model.net_class({'data':test_data})
     probs       = net.get_output()
     top_k_op    = tf.nn.in_top_k(probs, test_labels, top_k)
-    imagenet    = ImageNet(val_path, images_path)
+    imagenet    = ImageNet(val_path, images_path, model)
     correct     = 0
     count       = 0
     total       = len(imagenet)
     with tf.Session() as sesh:
         net.load(data_path, sesh)
-        for idx, (images, labels) in enumerate(imagenet.batches(BATCH_SIZE)):
+        for idx, (images, labels) in enumerate(imagenet.batches(model.batch_size)):
             correct += np.sum(sesh.run(top_k_op, feed_dict={test_data:images, test_labels:labels}))
             count += len(images)
             cur_accuracy = float(correct)*100/count
@@ -76,12 +75,12 @@ def main():
     model_index = 0 if len(args)==3 else int(args[3])
     if model_index>=len(examples.MODELS):
         print('Invalid model index. Options are:')
-        for idx, klass in enumerate(examples.MODELS):
-            print('%s: %s'%(idx, klass.__name__))
+        for idx, model in enumerate(examples.MODELS):
+            print('%s: %s'%(idx, model))
         exit(-1)  
-    Net = examples.MODELS[model_index]
-    print('Using model: %s'%(Net.__name__))
-    test_imagenet(Net, *args[:3])
+    model = examples.MODELS[model_index]
+    print('Using model: %s'%(model))
+    test_imagenet(model, *args[:3])
 
 if __name__ == '__main__':
     main()
