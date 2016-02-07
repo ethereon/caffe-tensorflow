bff6284cff48ca5ec8e3e77d495be96dfdf47e13
Saumitro Dasgupta <ethereon@xinverse.com>
2016-02-07 13:42:49 -0800

    Cleaned up argument parsing

--- convert.py
+++ convert.py
@@ -3,27 +3,28 @@
 import os
 import sys
 import numpy as np
+import argparse
 from kaffe import KaffeError
 from kaffe.tensorflow import TensorFlowTransformer
 
 def main():
-    args = sys.argv[1:]
-    if len(args) not in (3, 4, 5):
-        print('usage: %s path.prototxt path.caffemodel data-output-path [code-output-path.py] [phase=test]'%os.path.basename(__file__))
-        exit(-1)
-    def_path, data_path, data_out_path = args[:3]
-    src_out_path = args[3] if len(args)>=4 else None
-    phase = args[4] if len(args)>=5 else 'test'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('def_path', help='Model definition (.prototxt) path')
+    parser.add_argument('data_path', help='Model data (.caffemodel) path')
+    parser.add_argument('data_output_path', help='Converted data output path')
+    parser.add_argument('code_output_path', nargs='?', help='Save generated source to this path')
+    parser.add_argument('-p', '--phase', default='test', help='The phase to convert: test (default) or train')
+    args = parser.parse_args()
     try:
-        transformer = TensorFlowTransformer(def_path, data_path, phase = phase)
+        transformer = TensorFlowTransformer(args.def_path, args.data_path, phase=args.phase)
         print('Converting data...')
         data = transformer.transform_data()
         print('Saving data...')
-        with open(data_out_path, 'wb') as data_out:
+        with open(args.data_output_path, 'wb') as data_out:
             np.save(data_out, data)
-        if src_out_path is not None:
+        if args.code_output_path is not None:
             print('Saving source...')
-            with open(src_out_path, 'wb') as src_out:
+            with open(args.code_output_path, 'wb') as src_out:
                 src_out.write(transformer.transform_source())
         print('Done.')
     except KaffeError as err:
