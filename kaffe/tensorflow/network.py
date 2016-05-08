7f6d238cb7d55e38fd864b5e70a980ac7582dad5
Saumitro Dasgupta <ethereon@xinverse.com>
2016-05-08 00:19:39 -0700

    Commented the network class

--- kaffe/tensorflow/network.py
+++ kaffe/tensorflow/network.py
@@ -5,17 +5,18 @@ DEFAULT_PADDING = 'SAME'
 
 
 def layer(op):
+    '''Decorator for composable network layers.'''
 
     def layer_decorated(self, *args, **kwargs):
         # Automatically set a name if not provided.
         name = kwargs.setdefault('name', self.get_unique_name(op.__name__))
         # Figure out the layer inputs.
-        if len(self.inputs) == 0:
+        if len(self.terminals) == 0:
             raise RuntimeError('No input variables found for layer %s.' % name)
-        elif len(self.inputs) == 1:
-            layer_input = self.inputs[0]
+        elif len(self.terminals) == 1:
+            layer_input = self.terminals[0]
         else:
-            layer_input = list(self.inputs)
+            layer_input = list(self.terminals)
         # Perform the operation and get the output.
         layer_output = op(self, layer_input, *args, **kwargs)
         # Add to layer LUT.
@@ -31,15 +32,26 @@ def layer(op):
 class Network(object):
 
     def __init__(self, inputs, trainable=True):
-        self.inputs = []
+        # The input nodes for this network
+        self.inputs = inputs
+        # The current list of terminal nodes
+        self.terminals = []
+        # Mapping from layer names to layers
         self.layers = dict(inputs)
+        # If true, the resulting variables are set as trainable
         self.trainable = trainable
         self.setup()
 
     def setup(self):
-        raise NotImplementedError('Must be subclassed.')
+        '''Construct the network. '''
+        raise NotImplementedError('Must be implemented by the subclass.')
 
     def load(self, data_path, session, ignore_missing=False):
+        '''Load network weights.
+        data_path: The path to the numpy-serialized network weights
+        session: The current TensorFlow session
+        ignore_missing: If true, serialized weights for missing layers are ignored.
+        '''
         data_dict = np.load(data_path).item()
         for key in data_dict:
             with tf.variable_scope(key, reuse=True):
@@ -52,8 +64,11 @@ class Network(object):
                             raise
 
     def feed(self, *args):
+        '''Set the input(s) for the next operation by replacing the terminal nodes.
+        The arguments can be either layer names or the actual layers.
+        '''
         assert len(args) != 0
-        self.inputs = []
+        self.terminals = []
         for fed_layer in args:
             if isinstance(fed_layer, basestring):
                 try:
@@ -61,20 +76,26 @@ class Network(object):
                 except KeyError:
                     print self.layers.keys()
                     raise KeyError('Unknown layer name fed: %s' % fed_layer)
-            self.inputs.append(fed_layer)
+            self.terminals.append(fed_layer)
         return self
 
     def get_output(self):
-        return self.inputs[-1]
+        '''Returns the current network output.'''
+        return self.terminals[-1]
 
     def get_unique_name(self, prefix):
+        '''Returns an index-suffixed unique name for the given prefix.
+        This is used for auto-generating layer names based on the type-prefix.
+        '''
         ident = sum(t.startswith(prefix) for t, _ in self.layers.items()) + 1
         return '%s_%d' % (prefix, ident)
 
     def make_var(self, name, shape):
+        '''Creates a new TensorFlow variable.'''
         return tf.get_variable(name, shape, trainable=self.trainable)
 
     def validate_padding(self, padding):
+        '''Verifies that the padding is one of the supported ones.'''
         assert padding in ('SAME', 'VALID')
 
     @layer
@@ -89,20 +110,27 @@ class Network(object):
              relu=True,
              padding=DEFAULT_PADDING,
              group=1):
+        # Verify that the padding is acceptable
         self.validate_padding(padding)
+        # Get the number of channels in the input
         c_i = input.get_shape()[-1]
+        # Verify that the grouping parameter is valid
         assert c_i % group == 0
         assert c_o % group == 0
+        # Convolution for a given input and kernel
         convolve = lambda i, k: tf.nn.conv2d(i, k, [1, s_h, s_w, 1], padding=padding)
         with tf.variable_scope(name) as scope:
             kernel = self.make_var('weights', shape=[k_h, k_w, c_i / group, c_o])
             biases = self.make_var('biases', [c_o])
             if group == 1:
+                # This is the common-case. Convolve the input without any further complications.
                 conv = convolve(input, kernel)
             else:
+                # Split the input into groups and then convolve each of them independently
                 input_groups = tf.split(3, group, input)
                 kernel_groups = tf.split(3, group, kernel)
                 output_groups = [convolve(i, k) for i, k in zip(input_groups, kernel_groups)]
+                # Concatenate the groups
                 conv = tf.concat(3, output_groups)
             if relu:
                 bias = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape().as_list())
